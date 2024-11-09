f15309d6814e0349f333f90e21b5f759ba1265cd
Dan M <dsmiller95@gmail.com>
2024-11-09 16:34:56 -0600

    things

---
+++ Assets/Scripts/DisplayAction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayAction : MonoBehaviour
+{
+    public int playerIndex;
+    public Text displayText;
+        
+    private void Update()
+    {
+        displayText.text = GetPlayerActionDescription() ?? string.Empty;
+    }
+
+    private string GetPlayerActionDescription()
+    {
+        var gameManager = GameManager.GAME_MANAGER;
+        return null;
+    }
+}
---
+++ Assets/Scripts/DisplayResult.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayResult : MonoBehaviour
+{
+    public Text displayText;
+        
+    private void Update()
+    {
+        displayText.text = GetActionResultDescription() ?? string.Empty;
+    }
+
+    private string GetActionResultDescription()
+    {
+        var gameManager = GameManager.GAME_MANAGER;
+        return null;
+    }
+}
