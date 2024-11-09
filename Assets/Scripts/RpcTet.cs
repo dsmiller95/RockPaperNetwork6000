665c4a592e81fddf6b7384f0457662146c02efef
Dan M <dsmiller95@gmail.com>
2024-11-09 14:14:50 -0600

    go through demo https://docs-multiplayer.unity3d.com/netcode/current/tutorials/get-started-ngo/

---
+++ Assets/Scripts/RpcTet.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class RpcTest : NetworkBehaviour
+{
+    public override void OnNetworkSpawn()
+    {
+        //Only send an RPC to the server from the client that owns the NetworkObject of this NetworkBehaviour instance
+        if (!IsServer && IsOwner)
+        {
+            ServerOnlyRpc(0, NetworkObjectId);
+        }
+    }
+
+    [Rpc(SendTo.ClientsAndHost)]
+    void ClientAndHostRpc(int value, ulong sourceNetworkObjectId)
+    {
+        Debug.Log($"Client Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        if (IsOwner) //Only send an RPC to the owner of the NetworkObject
+        {
+            ServerOnlyRpc(value + 1, sourceNetworkObjectId);
+        }
+    }
+
+    [Rpc(SendTo.Server)]
+    void ServerOnlyRpc(int value, ulong sourceNetworkObjectId)
+    {
+        Debug.Log($"Server Received the RPC #{value} on NetworkObject #{sourceNetworkObjectId}");
+        ClientAndHostRpc(value, sourceNetworkObjectId);
+    }
+}
