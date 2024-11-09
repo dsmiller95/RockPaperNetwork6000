c7bb03d05cefa99c8e2bf5b40d24d58224a96812
Raphael Azcueta <rrazcueta@gmail.com>
2024-11-09 15:09:28 -0600

    WIP player action select

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -21,4 +21,22 @@ public class GameManager : NetworkBehaviour
             Destroy(gameObject);
         }
     }
+
+    [Rpc(SendTo.Server)]
+    public void ShieldRpc(ushort networkBehaviourId)
+    {
+        Debug.Log("Received shielding from " + networkBehaviourId);
+    }
+
+    [Rpc(SendTo.Server)]
+    public void MagicRpc(ushort networkBehaviourId)
+    {
+        Debug.Log("Received magicking from " + networkBehaviourId);
+    }
+
+    [Rpc(SendTo.Server)]
+    public void SwordRpc(ushort networkBehaviourId)
+    {
+        Debug.Log("Received swording from " + networkBehaviourId);
+    }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,4 +1,33 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class Player : NetworkBehaviour { }
+public class Player : NetworkBehaviour
+{
+    UnityEngine.KeyCode shield = KeyCode.A;
+    UnityEngine.KeyCode magic = KeyCode.S;
+    UnityEngine.KeyCode sword = KeyCode.D;
+
+    void Awake()
+    {
+        Debug.Log("Hi from " + NetworkBehaviourId);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(shield))
+        {
+            Debug.Log(NetworkBehaviourId + " is sending shielding");
+            GameManager.GAME_MANAGER.ShieldRpc(NetworkBehaviourId);
+        }
+        else if (Input.GetKeyDown(magic))
+        {
+            Debug.Log(NetworkBehaviourId + " is sending magicking");
+            GameManager.GAME_MANAGER.MagicRpc(NetworkBehaviourId);
+        }
+        else if (Input.GetKeyDown(sword))
+        {
+            Debug.Log(NetworkBehaviourId + " is sending swording");
+            GameManager.GAME_MANAGER.SwordRpc(NetworkBehaviourId);
+        }
+    }
+}
